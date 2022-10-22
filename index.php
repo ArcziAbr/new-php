c75476ac383180d757d2ef99bc68d74c83063f04
Artur <abrachamczyk.artur@gmail.com>
2022-10-22 16:33:12 +0200

    commit get to check

--- index.php
+++ index.php
@@ -1,11 +1,14 @@
 <?php
 
-declare(strict_types=1);
-
 namespace App;
 
+require_once('./src/view.php');
 include_once('./src/utils/debug.php');
-$action = $_GET['acion'] ?? null;
+
+$action = $_GET['action'] ?? null;
+
+$view = new View();
+$view->render($action);
 
 ?>
 
