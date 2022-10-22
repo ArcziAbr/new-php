bbb3fb868eabcdf873a6141c7b520b47066d0d2e
Artur <abrachamczyk.artur@gmail.com>
2022-10-22 15:18:23 +0200

    html to php

--- src/utils/debug.php
+++ src/utils/debug.php
@@ -1,2 +1,19 @@
 <?php
-include_once('.src/utils/debug.php');
+
+declare(strict_types=1);
+
+
+function dump($params)
+{
+    echo ('
+    <div style ="
+        display:inline-block;
+        background: lightgray;
+        border:1px solid gray;
+        padding:10px;
+    ">
+    <pre>
+    ');
+    print_r($params);
+    echo ('</pre></div><br>');
+}
